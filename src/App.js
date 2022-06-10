68fd45fa84b05cc304080a113af7f7f2ec0a4bb9
‘AMAYA <aamaya547@gmail.com>
2022-06-10 11:03:55 -0700

    added bee vs flower component, sized correctly

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import './App.css';
+// is the react import below necessary?
+import React from 'react';
+import { useState } from 'react';
 
 function App() {
+  const [beeSize, setBeeSize] = useState(50);
+  const [flowerSize, setFlowerSize] = useState(50);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="pollinator">
+        <p style={{ fontSize: `${beeSize}px` }}>🐝</p>
+        <div className="buttons">
+          <button>Sip Some Juice</button>
+          <button>Serve the Queen</button>
+        </div>
+      </div>
+      <div className="pollinator">
+        <p style={{ fontSize: `${flowerSize}px` }}>🌻</p>
+        <div className="buttons">
+          <button>Gimme that H2O</button>
+          <button>Time to Deadhead</button>
+        </div>
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/reload/i);
+  const linkElement = screen.getByText(/gimmie/i);
   expect(linkElement).toBeInTheDocument();
 });
