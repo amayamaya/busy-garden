dd5ec943598cf474e261111550499ac2b29a2e5a
‘AMAYA <aamaya547@gmail.com>
2022-06-10 15:50:14 -0700

    changed test for CI to pass

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/gimmie/i);
+  const linkElement = screen.getByText(/🌞/i);
   expect(linkElement).toBeInTheDocument();
 });
