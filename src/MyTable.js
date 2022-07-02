49465dec78e6d50844e766aef78773c063d94ed9
‘AMAYA <aamaya547@gmail.com>
2022-07-01 18:37:16 -0700

    light styling to list, installed victory

--- src/MyTable.js
+++ src/MyTable.js
@@ -1,27 +1,27 @@
 import MaterialTable from 'material-table';
-import { customers } from './data';
+import { customers } from './data.js';
+console.log(customers);
 
 export default function MyTable() {
-  console.log();
   return (
     <div style={{ maxWidth: '100%' }}>
       <MaterialTable
         columns={[
-          { title: 'COOL ID', field: 'id' },
-          { title: 'First Name', field: 'fist_name' },
+          { title: 'Username', field: 'id' },
+          { title: 'First Name', field: 'first_name' },
           { title: 'Special SurName', field: 'last_name' },
           { title: 'Email', field: 'email' },
           { title: 'Gender', field: 'gender' },
           { title: 'Computer Address Host', field: 'ip_address' },
         ]}
         data={customers}
-        title="My Special Title"
+        title="Floral Enthusiasts"
         options={{
           headerStyle: {
-            background: 'papayawhip',
+            background: 'mistyrose',
           },
           rowStyle: {
-            background: 'mistyrose',
+            background: 'thistle',
           },
         }}
       />
